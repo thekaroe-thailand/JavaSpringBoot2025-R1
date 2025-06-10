e80f468938b583ed9b4466f14071137305dc80e2
thekaroe-thailand <thekaroe@gmail.com>
2025-06-10 22:11:00 +0700

    endsale

---
+++ my-project/src/main/java/com/app/my_project/entity/BillSaleEntity.java
@@ -0,0 +1,85 @@
+package com.app.my_project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class BillSaleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @Column(name = "user_id")
+    private UserEntity user;
+
+    private Double inputMoney;
+    private Double discount;
+    private Double total;
+    private String status;
+    private LocalDate createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public Double getInputMoney() {
+        return inputMoney;
+    }
+
+    public void setInputMoney(Double inputMoney) {
+        this.inputMoney = inputMoney;   
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+    
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
